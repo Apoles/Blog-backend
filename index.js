32979933f9630fb884b2252482e658b433e6df2e
Apoles <Emine176gumus@gmail.com>
2022-01-06 15:30:14 +0300

    port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
 dotenv.config();
 
 app.use(cors());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 const CONNTECTION_URL = process.env.CONNTECTION_URL;
 
 app.use("/blog", blogRoute);
